b88eb89b1be3d71eb524355c8b508241d6b84404
peter <peter.rono@student.moringaschool.com>
2024-07-07 22:20:55 +0300

    Remove main element and add newHeader with victory message

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const newMain = document.querySelector("main#main");
+newMain.remove();
+//creates a new h1
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'RONO is the champion';//Peter Rono is the champion
+
+document.body.appendChild(newHeader);//append newHeader to body
